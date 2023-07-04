7b661b2bb8a0bda348ecd467d4ca865ae7275ccb
mori5321 <moriwm77@gmail.com>
2023-07-05 00:49:11 +0900

    react-konva

--- unison-client/src/App.tsx
+++ unison-client/src/App.tsx
@@ -1,9 +1,17 @@
 import './App.css'
 
+import { Stage, Layer, Circle } from 'react-konva';
+
 function App() {
   return (
     <>
-      <h1>Hello</h1>
+      <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Layer>
+          <Circle x={100} y={100} radius={50} fill="blue" />
+          <Circle x={200} y={100} radius={50} fill="yellow" />
+          <Circle x={300} y={100} radius={50} fill="red" />
+        </Layer>
+      </Stage>
     </>
   )
 }
