c022fdc8c1f3052f0ab6a7a7000f6e546f4de551
mori5321 <moriwm77@gmail.com>
2023-08-03 08:18:09 +0900

    feat: impl client to createEditor

--- unison-client/src/core/editor/client.ts
+++ unison-client/src/core/editor/client.ts
@@ -1,4 +1,37 @@
+import { Either, isLeft, left } from 'fp-ts/lib/Either';
 import { WorkerURL } from '../common/env';
+import * as t from 'io-ts';
+
+const editorResponse = t.type({
+  id: t.string,
+});
+
+type EditorResponse = t.TypeOf<typeof editorResponse>;
+
+
+type Phantomic<T, U extends string> = T & { _kind: U };
+
+// TODO: refactot it, make them reusable 
+type InvalidResponseError = Phantomic<{
+  message: string;
+}, '__invalidResponseError'>
+
+const InvalidResponseError = {
+  new: (message: string): InvalidResponseError => ({
+    message,
+    _kind: '__invalidResponseError',
+  })
+};
+
+type UnknownError = Phantomic<{}, '__unknownError'>
+
+const UnknownError = {
+  new: (): UnknownError => ({
+    _kind: '__unknownError',
+  })
+};
+
+type EditorResponseError = InvalidResponseError | UnknownError;
 
 export const ping = async () => {
   const res = await fetch(`${WorkerURL}/ping`);
@@ -8,3 +41,23 @@ export const ping = async () => {
     return new Error('ping failed');
   }
 };
+
+export const createEditor = async (): Promise<Either<EditorResponseError, EditorResponse>> => {
+  try {
+    const res = await fetch(`${WorkerURL}/editor`, { method: 'POST' });
+    const json = await res.json();
+    const response = editorResponse.decode(json);
+
+    if (isLeft(response)) {
+      const err = InvalidResponseError.new(
+        `Invalid response: ${JSON.stringify(response.left)}`
+      )
+      return left(err);
+    } else {
+      return response
+    }
+  } catch {
+    const err = UnknownError.new();
+    return left(err);
+  }
+}
---
+++ unison-client/src/utils/common/phantomic.ts
@@ -0,0 +1,2 @@
+export type Phantomic<T, U extends string> = T & { __tag: U };
+
