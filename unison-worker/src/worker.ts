023eb7d86ddd8a98eb0438b75c7e91409b34b325
mori5321 <moriwm77@gmail.com>
2023-08-06 16:02:25 +0900

    feat: handle preflight request

--- unison-worker/src/worker.ts
+++ unison-worker/src/worker.ts
@@ -5,8 +5,20 @@ export { Counter } from './durableObjects/counter';
 export { Editor } from './durableObjects/editor';
 
 export default {
-	async fetch(request: Request, env: Env, _ctx: ExecutionContext): Promise<Response> {
-		return handleRequest(request, env);
-	},
-};
+  async fetch(request: Request, env: Env, _ctx: ExecutionContext): Promise<Response> {
+    // handle preflight request
+    if (request.method === 'OPTIONS') {
+      return new Response(null, {
+        headers: {
+          'Access-Control-Allow-Origin': '*', // TODO: check Origin
+          'Access-Control-Allow-Methods': 'GET,HEAD,POST,OPTIONS',
+          'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+          'Access-Control-Allow-Credentials': 'true',
+          'Access-Control-Max-Age': '86400',
+        },
+      });
+    }
 
+    return handleRequest(request, env);
+  },
+};
